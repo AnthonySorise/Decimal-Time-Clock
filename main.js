826ec3abce50ee69a98a521dbf990eb031abf706
AnthonySorise <contact@anthonysorise.com>
2018-08-25 02:00:50 -0700

    formatting

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function generateFramerate(){
 }
 
 function updateFrame(){
-		updateHours();
-		updateMinutes();
-		updateSeconds();
+	updateHours();
+	updateMinutes();
+	updateSeconds();
 }
 
 function getDeciMS(){
-	var date = new Date();
-    var regMS = date.getTime() - date.setHours(0,0,0,0);
-    return regMS * 1.1574;// 100,000 decimal seconds / 86,400 standard seconds
+	var date  = new Date();
+	var regMS = date.getTime() - date.setHours(0,0,0,0);
+	return regMS * 1.1574;// 100,000 decimal seconds / 86,400 standard seconds
 }
 
 function updateHours(){
@@ -26,20 +26,20 @@ function updateHours(){
 
 function updateMinutes(){
 	//remove hours
-	var ms = getDeciMS();
+	var ms       = getDeciMS();
 	var toRemove = Math.floor(ms / 10000000);
-	var ms =      ms - (toRemove * 10000000);
+	var ms       = ms - (toRemove * 10000000);
 
-	var minutesTimesTen =    Math.floor(ms / 10000);
+	var minutesTimesTen = Math.floor(ms / 10000);
 	$(".minuteFill").removeClass().addClass("clockFill minuteFill minuteFill-" + minutesTimesTen);
 }
 
 function updateSeconds(){
 	//remove hours and minutes
-	var ms = getDeciMS();
+	var ms       = getDeciMS();
 	var toRemove = Math.floor(ms / 100000);
-	var ms =      ms - (toRemove * 100000);
+	var ms       = ms - (toRemove * 100000);
 
-	var seconds =    Math.floor(ms / 1000) * 10;//round to nearest decimal second
+	var seconds = Math.floor(ms / 1000) * 10;//round to nearest decimal second
 	$(".secondFill").removeClass().addClass("clockFill secondFill secondFill-" + seconds);
 }
