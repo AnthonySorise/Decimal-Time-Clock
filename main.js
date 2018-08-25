5e6a06f290a9cc8f64277c8fa615c457851221bf
AnthonySorise <contact@anthonysorise.com>
2018-08-25 16:17:18 -0700

    optimized js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	//initialize clock
+	updateSeconds();
+	updateMinutes();
+	updateHours();
+
+	//start framerate that updates clock
 	generateFramerate();
 })
 
@@ -8,9 +14,16 @@ function generateFramerate(){
 }
 
 function updateFrame(){
-	updateHours();
-	updateMinutes();
-	updateSeconds();
+	//update seconds every frame = every decimal time second
+	var secondsTimesTen = updateSeconds();
+	//update minutes every 10 frames = every 10 decimal time seconds
+	if(secondsTimesTen % 100 == 0){
+		var minutesTimesTen = updateMinutes();
+		//update hours every 100 frames = every 100 decimal time seconds (decimal time minute)
+		if(minutesTimesTen % 10 == 0){
+			updateHours();
+		}
+	}
 }
 
 function getDeciMS(){
@@ -19,9 +32,15 @@ function getDeciMS(){
 	return regMS * 1.1574;// 100,000 decimal seconds / 86,400 standard seconds
 }
 
-function updateHours(){
-	var hourTimesTen = Math.floor(getDeciMS() / 100000);
-	$(".hourFill").removeClass().addClass("clockFill hourFill hourFill-" + hourTimesTen);
+function updateSeconds(){
+	//remove hours and minutes
+	var ms       = getDeciMS();
+	var toRemove = Math.floor(ms / 100000);
+	var ms       = ms - (toRemove * 100000);
+
+	var secondsTimesTen = Math.floor(ms / 1000) * 10;//round to nearest decimal second
+	$(".secondFill").removeClass().addClass("clockFill secondFill secondFill-" + secondsTimesTen);
+	return secondsTimesTen
 }
 
 function updateMinutes(){
@@ -32,14 +51,10 @@ function updateMinutes(){
 
 	var minutesTimesTen = Math.floor(ms / 10000);
 	$(".minuteFill").removeClass().addClass("clockFill minuteFill minuteFill-" + minutesTimesTen);
+	return minutesTimesTen;
 }
 
-function updateSeconds(){
-	//remove hours and minutes
-	var ms       = getDeciMS();
-	var toRemove = Math.floor(ms / 100000);
-	var ms       = ms - (toRemove * 100000);
-
-	var seconds = Math.floor(ms / 1000) * 10;//round to nearest decimal second
-	$(".secondFill").removeClass().addClass("clockFill secondFill secondFill-" + seconds);
+function updateHours(){
+	var hourTimesTen = Math.floor(getDeciMS() / 100000);
+	$(".hourFill").removeClass().addClass("clockFill hourFill hourFill-" + hourTimesTen);
 }
